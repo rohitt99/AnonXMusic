d1fa34c82d3de907df6e0a4e5d5e6cbb069788a1
NhoetKyaiteKaungLayy <ohmyqueenmedusa20s@gmail.com>
2024-06-05 18:09:25 +0630

    Update play.py
    Signed-off-by: NhoetKyaiteKaungLayy <ohmyqueenmedusa20s@gmail.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -66,6 +66,27 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
+        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="𖹭 ᴏᴡɴᴇʀ 𖹭", url=f"https://t.me/NhoeKyaiteKaungLayy"
+            ),
+            InlineKeyboardButton(
+                text="𖹭 ᴄʜᴀᴛ 𖹭", url=f"https://t.me/seriousvs_version10"
+            ),
+        ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
