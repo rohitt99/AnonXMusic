13af484584cc0cf0146c9683f2d114e31614e95f
NhoetKyaiteKaungLayy <ohmyqueenmedusa20s@gmail.com>
2024-06-05 18:42:27 +0630

    Update play.py
    Signed-off-by: NhoetKyaiteKaungLayy <ohmyqueenmedusa20s@gmail.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -67,19 +67,6 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-        ],
-        [
             InlineKeyboardButton(
                 text="𖹭 ᴏᴡɴᴇʀ 𖹭", url=f"https://t.me/NhoeKyaiteKaungLayy"
             ),
@@ -100,6 +87,14 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(
+                text="𖹭 ᴏᴡɴᴇʀ 𖹭", url=f"https://t.me/NhoeKyaiteKaungLayy"
+            ),
+            InlineKeyboardButton(
+                text="𖹭 ᴄʜᴀᴛ 𖹭", url=f"https://t.me/seriousvs_version10"
+            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
