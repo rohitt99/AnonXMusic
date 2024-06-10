1076afb48574b93195c07aa46b3e59858d4f57d6
NhoetKyaiteKaungLayy <ohmyqueenmedusa20s@gmail.com>
2024-06-10 09:20:17 +0630

    Update settings.py
    Signed-off-by: NhoetKyaiteKaungLayy <ohmyqueenmedusa20s@gmail.com>

--- AnonXMusic/plugins/bot/settings.py
+++ AnonXMusic/plugins/bot/settings.py
@@ -73,7 +73,7 @@ async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
         InputMediaVideo("https://te.legra.ph/file/2b8291641c7f35a9bee51.mp4"),
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
+            [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settings_back_helper")]]
         ),
     )
 
