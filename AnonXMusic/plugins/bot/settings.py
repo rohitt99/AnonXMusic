c07d486172a725d16b7b607cecc55c4b00eb9148
NhoetKyaiteKaungLayy <ohmyqueenmedusa20s@gmail.com>
2024-06-10 10:15:29 +0630

    Update settings.py
    Signed-off-by: NhoetKyaiteKaungLayy <ohmyqueenmedusa20s@gmail.com>

--- AnonXMusic/plugins/bot/settings.py
+++ AnonXMusic/plugins/bot/settings.py
@@ -36,7 +36,7 @@ from AnonXMusic.utils.inline.settings import (
     vote_mode_markup,
 )
 from AnonXMusic.utils.inline.start import private_panel
-from config import BANNED_USERS, OWNER_ID MUSIC_BOT_NAME, START_IMG_URL
+from config import BANNED_USERS,
 
 
 @app.on_message(
